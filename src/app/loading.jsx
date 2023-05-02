832a214ae8ac0b0565d9b3fcebe9c4fe322ce9fe
Deekshith <deekshithreddy1317@gmail.com>
2023-05-02 23:18:04 +0530

    loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center">
+      <img className="h-6" src="spinner.svg" alt="loading" />
+    </div>
+  );
+}
