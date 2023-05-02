7f096da7f3e965cdf84b2c513f5b1bc1037e15a9
Deekshith <deekshithreddy1317@gmail.com>
2023-05-02 16:29:51 +0530

    created about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-6">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
+        laborum iure culpa eligendi libero dolores quis, omnis iste, aut amet
+        harum, aliquid totam magni quos voluptate reiciendis itaque delectus
+        quasi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
+        iste fuga accusantium modi beatae neque ut eaque ipsum, tenetur magnam
+        laborum minima exercitationem culpa pariatur autem provident fugit
+        ducimus magni? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Quasi tenetur qui at fugit cupiditate hic esse nulla asperiores magni
+        facere expedita, nesciunt eligendi vitae, totam libero in odit deserunt
+        modi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
+        laborum iure culpa eligendi libero.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
+        laborum iure culpa eligendi libero dolores quis, omnis iste, aut amet
+        harum, aliquid totam magni quos voluptate reiciendis itaque delectus
+        quasi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta
+        dolorum reiciendis enim quaerat aliquam architecto dignissimos
+        voluptatum aliquid. Ab iusto ut, saepe quisquam illo cumque ipsam
+        voluptates incidunt eveniet eligendi.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
+        laborum iure culpa eligendi libero dolores quis, omnis iste, aut amet
+        harum, aliquid totam magni quos voluptate reiciendis itaque delectus
+        quasi.
+      </p>
+    </div>
+  );
+}
